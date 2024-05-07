a474d0f6ca420fcf123f6a11282273bd146aefea
Emanhaile <emanhaile06@gmail.com>
2024-05-07 14:15:11 -0400

    banner

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
-
+import Banner from '../../components/Banner/Banner'
 function Home() {
   return (
     
       <>
         <Header/>
+        <Banner/>
         <Footer/>
       </>
     
--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -4,3 +4,4 @@ const instance = axios.create({
 });
 
 export default instance
+
